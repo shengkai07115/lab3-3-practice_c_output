f26490e88109aafb403daf631c7cfc5a5b411f24
shengkai07115 <144308855+shengkai07115@users.noreply.github.com>
2023-09-21 11:21:52 +0800

    Add files via upload

---
+++ lab3-3-practice_c_output/3-3-practice_c_output.cpp
@@ -0,0 +1,9 @@
+//3-3-practice_c_output.cpp
+#include <iostream>
+using namespace std;
+int main () {
+	int student_id = 1204226;
+	char class_name = 'B';
+	printf("My student ID is %d.\n I am one of the students in Daan Elect. class %c.\n", student_id, class_name);
+	system("Pause");
+}
